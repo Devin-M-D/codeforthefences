77eda8bd495459a2694a26c7991c630161808624
Devin-M-D <downer.devin@gmail.com>
2020-08-06 23:01:58 -0500

    housekeeping
    - index styles for shy scrolling
    - added drawer pane and flex carousel to cargo hold load
    - added drawer pane widget
    - added carousel container to home page

--- client/components/big5/cargoHold/cargoHold.js
+++ client/components/big5/cargoHold/cargoHold.js
@@ -7,4 +7,12 @@ async function loadCargoHold(){
   var recipeCard = await cDI.asyncGet("components/projectWidgets/recipeCard/recipeCard.html")
   $("#cargoHold").append(recipeCard)
   await loadWidgetRecipeCard()
+
+  var drawerPane = await cDI.asyncGet("components/genericWidgets/drawerPane/drawerPane.html")
+  $("#cargoHold").append(drawerPane)
+  await loadWidgetDrawerPane()
+
+  var flexCarousel = await cDI.asyncGet("components/genericWidgets/flexCarousel/flexCarousel.html")
+  $("#cargoHold").append(flexCarousel)
+  await loadFlexCarousel()
 }
