07ef80c6c59023f812be103ab36bd0c02a2cbc9b
Devin-M-D <downer.devin@gmail.com>
2021-08-01 18:05:43 -0500

    darkRoom refactor
    - touch up css and readability of carousel
    - bugfix for user routes
    
    NEXT UP:
    - extract queryBuilder

--- server/services/userService.js
+++ server/services/userService.js
@@ -1,6 +1,6 @@
 var queryBuilder = require('../utils/queryBuilder')
 var userQueries = require("../queries/user/userQueries")
-
+var DI = require("../foundation/DICore")
 module.exports = {
   getAll: async () => {
     return await queryBuilder.quickRun(userQueries.getAll)
